a3440e1d953be7e2166a7b13bb998f7777ccb6dd
Steve Matt <161694181+lainadaina@users.noreply.github.com>
2024-02-29 09:47:18 +0200

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,22 @@
+public override void WriteHeaders ()
+		{
+			headers.WriteLine ();
+			headers.WriteLine ("/** This override the default hashing computation (defined in NSObject Protocol)");
+			headers.WriteLine (" * https://developer.apple.com/reference/objectivec/1418956-nsobject/1418859-hash?language=objc");
+			headers.WriteLine (" */");
+			base.WriteHeaders ();
+		}
+
+#if DEBUG
+			Entries.Add (line);
+#endif
+		}
+
+		public static void Dump ()
+		{
+#if DEBUG
+			Entries.ForEach (l => Console.WriteLine (l));
+#endif
+		}
+	}
+}
